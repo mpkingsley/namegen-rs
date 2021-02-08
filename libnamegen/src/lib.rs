c4ac981cd92ef6869cf2b18e73d7111172767a7d
Matt Kingsley <m.p.kingsley@gmail.com>
2021-02-08 17:48:13 -0500

    Continue Modular process

--- libnamegen/src/lib.rs
+++ libnamegen/src/lib.rs
@@ -5,18 +5,59 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
-mod rule;
 mod xmlparse;
+mod rule;
 
-use rule::Rule;
-use oorandom::Rand32;
-
+mod mchain {
+#![warn(unused_mut)]
 struct Chain {
     id: u32, //node id
-    xmlfile: &'static str, //filename of xml file
-    chain: markov::Chain<char>,
+    xmlfile: String, //filename of xml file
+    markov: markov::Chain<char>,
     trained: bool
 }
+ 
+impl Chain {
+    fn new(filename:&str,listid:u32, order:usize) -> Self {
+        let mut markov = markov::Chain::of_order(order);
+        Chain {
+            id: listid,
+            xmlfile: String::from(filename),
+            markov,
+            trained: false
+        }
+    }
+
+
+    fn train(&mut self, data:&[String]) -> &mut Self {
+        let mut view: Vec<char> = Vec::new();
+        for a in data.iter() {
+            let strview:Vec<char> = a.as_str().trim().chars().collect();
+            for c in strview{
+                view.push(c);
+            }
+        self.markov.feed(view.drain(0..));
+     }
+     self.trained = true;
+     self
+    }
+
+    fn get_id(&self) -> (u32,String)  {
+        (self.id,self.xmlfile.clone())
+    }
+    
+    fn is_trained(&self) -> bool {
+        self.trained
+    }
+
+}
+
+}
+use rule::Rule;
+use oorandom::Rand32;
+use xmlparse::Xmldoc;
+
+
 
 
 
--- libnamegen/src/xmlparse.rs
+++ libnamegen/src/xmlparse.rs
@@ -1,8 +1,7 @@
 use roxmltree::Document;
 
-
 pub struct Xmldoc<'a> {
-    file: &'a Document<'a>,
+    file: Document<'a>,
     filename: &'a str
 }
 pub enum Error {
@@ -25,31 +24,85 @@ impl From<std::io::Error> for Error {
 
 impl<'a> Xmldoc<'a> {
 
- fn new(filename: &'a str ) -> Result<Self, Error> {
-    let data = match read_in(filename){
-        Ok(String) => String,
-        Err(err) => return Err(Error::FileReadError(err)),
-    };
-    let xml = data.as_str().clone();
-    let file = match Document::parse(xml){
-        Ok(Document) => Document,
-        Err(err) => return Err(Error::DocumentError(err)),
-    };
-    
-    let doc = Xmldoc{file:&'a file,filename};
+ fn new(data: &'a str, filename:&'a str ) -> Result<Self, Error> {
 
-    
+    let file: Document<'a>;
+    file = match Document::parse(data){
+                Ok(document) => document,
+                Err(err) => return Err(Error::DocumentError(err)),
+            };
+    let doc = Xmldoc{file:file,filename};
+
     Ok(doc)
+ 
  }
 
+ fn get_rulesets(&self) -> Vec<u32> {
+    let mut ids:Vec<u32> = vec!();
+    let xml = &self.file;
+    for node in xml.descendants(){
+        if node.tag_name().name().to_lowercase() == "ruleset" {
+            ids.push(node.id().get());
+        }
+    }
+    ids
+  }
+
+  fn find_id(&self, idref: &str) -> u32 {
+    use roxmltree::NodeId;
+    let xml = &self.file;
+    let lists = &self.get_lists();
+    let rulesets = &self.get_rulesets();
+ 
+    for id in lists {
+        let list =  xml.get_node(NodeId::from(*id)).unwrap();
+        if list.attribute("id").unwrap_or("none") == idref {
+            return *id
+        };
+    }
+    for id in rulesets {
+        let rule =  xml.get_node(NodeId::from(*id)).unwrap();
+         if rule.attribute("id").unwrap_or("none") == idref {
+             return *id
+         };
+     }
+  0 //return 0 if not found
+ }
+
+ fn get_lists(&self) -> Vec<u32> {
+    let xml = &self.file;
+    let mut ids:Vec<u32> = vec!();
+    for node in xml.descendants(){
+        if node.tag_name().name().to_lowercase() == "list" {
+            ids.push(node.id().get());
+        }
+    }
+    ids
+  }
+
+  fn get_data(&self, listid: u32) -> Vec<String> {
+    let mut data:Vec<String> = Vec::new();
+    let xml = &self.file;
+    let list = xml.get_node(roxmltree::NodeId::from(listid)).unwrap();
+    for value in list.children(){
+        if value.tag_name().name().to_lowercase() == "value" {
+            let text = value.text().unwrap_or("Parsing Err").trim();
+            data.push(String::from(text));
+           // println!("Found data: {: }", text);
+        }
+        /* else {
+            println!("Tag found with name: {: }",(value.tag_name().name().to_lowercase()));
+        }; */
+    }
+data
 }
 
-fn read_in(filename:&str) -> Result<String, std::io::Error> {
-    
-
-    return std::fs::read_to_string(filename)
-
 }
 
+pub fn read_file(filename:&str) -> Result<String, Error> {
+    match std::fs::read_to_string(filename){
+       Ok(data) => return Ok(data),
 
-
+       Err(error) => return Err(Error::FileReadError(error)),
+   };
+}
