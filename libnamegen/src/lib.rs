41ffe0d2dafd3ed777cd3ecac1707e067bb27f30
Matt Kingsley <m.p.kingsley@gmail.com>
2021-02-11 12:26:12 -0500

    continued development

--- libnamegen/src/lib.rs
+++ libnamegen/src/lib.rs
@@ -1,61 +1,46 @@
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
 }
 mod xmlparse;
-mod rule;
+mod rule{
 
-mod mchain {
-#![warn(unused_mut)]
-struct Chain {
-    id: u32, //node id
-    xmlfile: String, //filename of xml file
-    markov: markov::Chain<char>,
-    trained: bool
-}
- 
-impl Chain {
-    fn new(filename:&str,listid:u32, order:usize) -> Self {
-        let mut markov = markov::Chain::of_order(order);
-        Chain {
-            id: listid,
-            xmlfile: String::from(filename),
-            markov,
-            trained: false
-        }
-    }
-
-
-    fn train(&mut self, data:&[String]) -> &mut Self {
-        let mut view: Vec<char> = Vec::new();
-        for a in data.iter() {
-            let strview:Vec<char> = a.as_str().trim().chars().collect();
-            for c in strview{
-                view.push(c);
-            }
-        self.markov.feed(view.drain(0..));
-     }
-     self.trained = true;
-     self
-    }
-
-    fn get_id(&self) -> (u32,String)  {
-        (self.id,self.xmlfile.clone())
-    }
-    
-    fn is_trained(&self) -> bool {
-        self.trained
-    }
-
+#[derive(Clone,Copy)]
+pub struct Rule<'a> {
+   idref: &'a[String], //idref string
+    weight: u32, //weight for each rule
+    style: &'a str,    //style with formatting
 }
 
+impl<'a> Rule<'a>{
+ fn new(idref: &'a [String], weight:u32, style: &'a str) -> Rule<'a> {
+     Rule{idref: idref.clone(), weight: weight.clone(), style: style.clone() }
+ }
+
+ pub fn get_style(&self) -> &str{
+    self.style.clone()
 }
+
+pub fn get_idref(&self) -> &[String] {
+    self.idref.clone()
+}
+
+pub fn get_weight(&self) -> u32 {
+    self.weight
+}
+}
+
+
+}
+mod mchain;
 use rule::Rule;
 use oorandom::Rand32;
 use xmlparse::Xmldoc;
+
 
 
 
