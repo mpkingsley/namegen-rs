69301f3cf8c296ece63c78dfb5330a630d85b92a
Matt Kingsley <m.p.kingsley@gmail.com>
2021-06-23 09:35:17 -0400

    continued modularization

--- libnamegen/src/lib.rs
+++ libnamegen/src/lib.rs
@@ -1,3 +1,5 @@
+#[crate_name="libnamegen"]
+
 #[cfg(test)]
 mod tests {
 
@@ -7,62 +9,46 @@ mod tests {
     }
 }
 mod xmlparse;
-mod rule{
-
-#[derive(Clone,Copy)]
-pub struct Rule<'a> {
-   idref: &'a[String], //idref string
-    weight: u32, //weight for each rule
-    style: &'a str,    //style with formatting
-}
-
-impl<'a> Rule<'a>{
- fn new(idref: &'a [String], weight:u32, style: &'a str) -> Rule<'a> {
-     Rule{idref: idref.clone(), weight: weight.clone(), style: style.clone() }
- }
-
- pub fn get_style(&self) -> &str{
-    self.style.clone()
-}
-
-pub fn get_idref(&self) -> &[String] {
-    self.idref.clone()
-}
-
-pub fn get_weight(&self) -> u32 {
-    self.weight
-}
-}
-
-
-}
 mod mchain;
-use rule::Rule;
+
+
 use oorandom::Rand32;
-use xmlparse::Xmldoc;
+use xmlparse::{*};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn select_rule<'a>(ruleset: &'a[Rule<'a>]) -> &Rule<'a> {
+    ///selects a rule by a weighted rng. 
+pub fn select_rule(ruleset: xmlparse::Ruleset) -> Rule {
   //  let mut weights:Vec<usize> = Vec::new();  //may not need this 
-    let mut index:Vec<&Rule<'a>> = Vec::new();
+    let mut index:Vec<Rule> = Vec::new();
     let seed = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_nanos() as u64;
     let mut rng = Rand32::new(seed);
-    for r in ruleset {
+    for r in ruleset.get_rules() {
        // weights.push(r.get_weight() as usize);
         let mut i = r.get_weight();
         while i > 0 {
-            index.push(&r);
+            index.push(r.clone());
             i= i - 1;
-        }
-
-      
+        }      
     }
-
-   return index[rng.rand_range(0 .. index.len() as u32 ) as usize];  
+   return index[rng.rand_range(0.. index.len() as u32 ) as usize].clone();  
   }
 
+  ///Seletcts an unweighted item randomly from a list. 
+  pub fn select_item(list: &[u32]) -> u32 {
+    let seed = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_nanos() as u64;
+    let mut rng = Rand32::new(seed);
+    return list[rng.rand_range(0.. list.len() as u32) as usize];
+  }
 
+  fn contruct_doc<'a>(filename:&'a str) -> Result<Vec<Ruleset>,xmlparse::Error> {
+      let mut generators:Vec<Ruleset> = Vec::new();
+      let xml = Xmldoc::new(filename);
+      if xml.is_err(){
+          return Err(xml.unwrap_err());
+      }
+      let nodes = xml.unwrap().get_generators();
 
-
+    return Ok(generators);
+  }
 
 
